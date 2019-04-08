35221aba88215267e056aaeca52cee65c07ea43e
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-04-08 16:08:52 -0600

    opened repo for AgEagle eng assessment, including html file, style and app.js with initial map from mapbox

---
+++ app.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaHJvc2VtY21haG9uIiwiYSI6ImNqdTh2eDB6NjI5ajk0ZHNhb25kdGhrdHIifQ.5_ze2MlAP5E8gD3mxoJA6Q';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-74.50, 40], // starting position
+    zoom: 9 // starting zoom
+    });
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
